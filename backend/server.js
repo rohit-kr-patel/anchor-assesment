8e698ed8d9aeab5983d9f27cf2997ccf3b37756e
rohit-ku-patel <rp58007@gmail.com>
2025-01-28 02:12:23 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin:  "*",
+  origin:  "http://localhost:5173/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
